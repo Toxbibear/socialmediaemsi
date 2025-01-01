1072a1957c2d2babb040f94e6e01afaa08c42716
Toxbibear <iliasse.ilia@gmail.com>
2025-01-01 16:29:31 +0000

    first commit

---
+++ lib/pages/login_page.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+import 'package:socialmediaemsi/components/my_button.dart';
+import 'package:socialmediaemsi/components/my_textfield.dart';
+
+class LoginPage extends StatelessWidget {
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+
+  LoginPage({super.key});
+
+  void login() {}
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.surface,
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(25.0),
+          child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+            Icon(
+              Icons.person,
+              size: 80,
+              color: Theme.of(context).colorScheme.inversePrimary,
+            ),
+            const SizedBox(height: 25),
+            Text("M I N I M A L",
+                style: TextStyle(
+                  fontSize: 20,
+                )),
+            const SizedBox(height: 50),
+            MyTextfield(
+              hintText: "Email",
+              obscureText: false,
+              controller: emailController,
+            ),
+            const SizedBox(height: 10),
+            MyTextfield(
+              hintText: "Password",
+              obscureText: true,
+              controller: passwordController,
+            ),
+            const SizedBox(height: 10),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Text("Forgot password?",
+                    style: TextStyle(
+                        color: Theme.of(context).colorScheme.inversePrimary)),
+              ],
+            ),
+            const SizedBox(height: 25),
+            MyButton(
+              text: "Login",
+              onTap: login,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text("don't have an account?",
+                    style: TextStyle(
+                        color: Theme.of(context).colorScheme.inversePrimary)),
+                GestureDetector(
+                  onTap: () {},
+                  child: Text(
+                    "Register",
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                ),
+              ],
+            )
+          ]),
+        ),
+      ),
+    );
+  }
+}
