9e2b72c6161fd9355e15bb61e353a82308e75d0a
Toxbibear <iliasse.ilia@gmail.com>
2025-01-02 02:02:37 +0000

    .

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,4 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
+// import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:socialmediaemsi/components/my_drawer.dart';
 import 'package:socialmediaemsi/components/my_list_tile.dart';
@@ -100,7 +100,7 @@ class HomePage extends StatelessWidget {
                       //get data from each post
                       String message = post['PostMessage'];
                       String userEmail = post['UserEmail'];
-                      Timestamp timestamp = post['PostTimeStamp'];
+                      // Timestamp timestamp = post['PostTimeStamp'];
 
                       return MyListTile(title: message, subTitle: userEmail);
                     },
