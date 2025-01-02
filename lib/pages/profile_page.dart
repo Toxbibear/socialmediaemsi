691cdc305a2c9d9d21533e6512a194e02b6405fb
Toxbibear <iliasse.ilia@gmail.com>
2025-01-02 00:13:54 +0000

    register stored in firestore

--- lib/pages/profile_page.dart
+++ lib/pages/profile_page.dart
@@ -1,7 +1,22 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class ProfilePage extends StatelessWidget {
-  const ProfilePage({super.key});
+  ProfilePage({super.key});
+
+  //current logged user
+
+  final User? currentUser = FirebaseAuth.instance.currentUser;
+
+  //futur fetch user deails
+
+  Future<DocumentSnapshot<Map<String, dynamic>>> getUserDetails() async {
+    return await FirebaseFirestore.instance
+        .collection("Users")
+        .doc(currentUser!.email)
+        .get();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -9,7 +24,45 @@ class ProfilePage extends StatelessWidget {
       appBar: AppBar(
         title: Text("Profile"),
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        elevation: 0,
       ),
+      backgroundColor: Theme.of(context).colorScheme.surface,
+      body: FutureBuilder<DocumentSnapshot<Map<String, dynamic>>>(
+          future: getUserDetails(),
+          builder: (context, snapshot) {
+            //loading
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+
+            //error
+            else if (snapshot.hasError) {
+              return Center(
+                child: Text("Error: ${snapshot.error}"),
+              );
+            }
+
+            //data
+            else if (snapshot.hasData) {
+              Map<String, dynamic>? user = snapshot.data!.data();
+
+              return Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(user!['email']),
+                    Text(user['username']),
+                  ],
+                ),
+              );
+            } else {
+              return Center(
+                child: Text("No data"),
+              );
+            }
+          }),
     );
   }
 }
--- lib/pages/register_page.dart
+++ lib/pages/register_page.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:socialmediaemsi/components/my_button.dart';
@@ -42,11 +43,31 @@ class _RegisterPageState extends State<RegisterPage> {
           email: emailController.text,
           password: passwordController.text,
         );
-        Navigator.pop(context);
+
+        createUserDocument(userCredential);
+
+        if (mounted) {
+          Navigator.pop(context);
+        }
       } on FirebaseAuthException catch (e) {
-        Navigator.pop(context);
-        displayMessageToUser(e.code, context);
+        if (mounted) {
+          Navigator.pop(context);
+          displayMessageToUser(e.code, context);
+        }
       }
+    }
+  }
+
+  //create createUserDocument
+  Future<void> createUserDocument(UserCredential? userCredential) async {
+    if (userCredential != null && userCredential.user != null) {
+      await FirebaseFirestore.instance
+          .collection("Users")
+          .doc(userCredential.user!.email)
+          .set({
+        'email': userCredential.user!.email,
+        'username': usernameController.text,
+      });
     }
   }
 
